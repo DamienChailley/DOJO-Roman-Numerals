43ccf756252036dd37e40ba1b7d09e117a453273
DamienChailley <playwithcube@gmail.com>
2023-06-18 14:49:28 +0200

    Add base template

---
+++ DojoRomanNumerals/RomanNumeralsConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace DojoRomanNumerals
+{
+    public static class RomanNumeralsConverter
+    {
+        public static string ToRomanNumerals(int number)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ DojoRomanNumeralsUnitTests/RomanNumerals.cs
@@ -0,0 +1,18 @@
+using DojoRomanNumerals;
+
+namespace DojoRomanNumeralsUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
